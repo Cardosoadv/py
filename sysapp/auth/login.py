5069ea348b93639ab173e6216c104b3503f26540
Fabiano Dias Cardoso <97191708+Cardosoadv@users.noreply.github.com>
2023-07-24 14:35:58 -0300

    Update login.py

--- sysapp/auth/login.py
+++ sysapp/auth/login.py
@@ -1,12 +1,19 @@
 import bcrypt
 
-def hashPassword(password):
-
+def hash_password(password):
     # Adding the salt to password
     salt = bcrypt.gensalt()
     # Hashing the password
     hashed = bcrypt.hashpw(password.encode(), salt)
-    hashedPassword=[]
-    hashedPassword.append(salt)
-    hashedPassword.append(hashed)
-    return hashedPassword
+    hashed_password = []
+    hashed_password.append(salt)
+    hashed_password.append(hashed)
+    return hashed_password
+
+
+def verify_hash(hashs, password, password_input):
+    hashed_password = bcrypt.hashpw(password_input.encode(),hashs)
+    if password == hashed_password:
+        return True
+    else:
+        return False
